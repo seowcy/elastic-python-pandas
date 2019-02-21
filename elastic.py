eb1f9fab75977b1779acb28429d0d76c57ba864a
Seow Chun Yong <seowcy@gmail.com>
2019-02-21 23:31:35 +0800

    Reduced indentation
    Indentation was overzealous; Reduced it to PEP8 standards.

--- elastic.py
+++ elastic.py
@@ -20,50 +20,50 @@ Args:
 
 """
 	body = {
-			"query": {
-    				"match": {
-      					column: query
-    				}
-  			}
+		"query": {
+			"match": {
+				column: query
+			}
 		}
+	}
 	response = es.search(index=index,body=body,request_timeout=120)
 	total_hits = response["hits"]["total"]
 	if size == None:
 		size = total_hits
 	if size <= 10000:
 		body = {
-                        	"size": size,
-                        	"query": {
-                                	"match": {
-                                        	column: query
-                                	}
-                        	}
-                        }
-                response = es.search(index=index,body=body,request_timeout=120)
+        	"size": size,
+            	"query": {
+                	"match": {
+                    	column: query
+                	}
+            	}
+        }
+	    response = es.search(index=index,body=body,request_timeout=120)
 		return pd.DataFrame([response["hits"]["hits"][i]["_source"] for i in range(size)])
 	if total_hits <= 10000:
 		body = {
-                        	"size": total_hits,
-                        	"query": {
-                                	"match": {
-                                        	column: query
-                                	}
-                        	}
-                        }
-                response = es.search(index=index,body=body,request_timeout=120)
+        	"size": total_hits,
+            	"query": {
+                	"match": {
+                    	column: query
+                	}
+            	}
+        }
+        response = es.search(index=index,body=body,request_timeout=120)
 		return pd.DataFrame([response["hits"]["hits"][i]["_source"] for i in range(total_hits)])
 	result = []
 	body = {
-                        "size": 10000,
-                        "query": {
-                                "match": {
-                                        column: query
-                                }
-                        }
-               }
-        response = es.search(index=index,body=body,scroll="10s",request_timeout=120)
+        "size": 10000,
+            "query": {
+                "match": {
+                    column: query
+                }
+            }
+   }
+    response = es.search(index=index,body=body,scroll="10s",request_timeout=120)
 	response_hits = len(response["hits"]["hits"])
-        result.append(pd.DataFrame([response["hits"]["hits"][i]["_source"] for i in range(response_hits)]))
+    result.append(pd.DataFrame([response["hits"]["hits"][i]["_source"] for i in range(response_hits)]))
 	scroll_id = response["_scroll_id"]
 	for i in range(10000,size,10000):
 		response = es.scroll(scroll_id=scroll_id,scroll="10s")
@@ -72,4 +72,3 @@ Args:
 	result = pd.concat(result,axis=0,ignore_index=True)
 	result["@timestamp"] = pd.to_datetime(result["@timestamp"])
 	return result.sort_values("@timestamp", ascending=False).reset_index(drop=True)
-		
