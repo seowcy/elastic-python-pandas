5f1ceebf4be4c24399355fc7956affaf8099ea1f
Seow Chun Yong <seowcy@gmail.com>
2019-09-04 22:23:02 +0800

    Update elastic.py
    Added elastic_get_indices
    Added elastic_query_range
    Added elastic_simple_aggs
    
    Updated elastic_simple_query to work with Elasticsearch 7.x

--- elastic.py
+++ elastic.py
@@ -7,6 +7,20 @@ import json
 ELASTIC_HOST = "127.0.0.1"
 es = Elasticsearch(ELASTIC_HOST)
 
+
+def elastic_get_indices(all=False):
+    """
+Returns a list of all indices (hidden or not hidden) in the elasticsearch cluster.
+
+Args:
+    all: if True, returns hidden indices as well (i.e. those prefixed with a period) (default: False)
+
+    """
+    if all:
+        return sorted(es.cat.indices(h=["index"]).split())
+    else:
+        return sorted([i for i in es.cat.indices(h=["index"]).split() if i[0] != '.'])
+
 def elastic_simple_query(index,column,query,size=None):
     """
 Returns a pandas DataFrame of specified size from the specified index with a search query
@@ -27,7 +41,7 @@ Args:
         }
     }
     response = es.search(index=index,body=body,request_timeout=120)
-    total_hits = response["hits"]["total"]
+    total_hits = response["hits"]["total"]["value"]
     if size == None:
         size = total_hits
     if size <= 10000:
@@ -72,3 +86,171 @@ Args:
     result = pd.concat(result,axis=0,ignore_index=True)
     result["@timestamp"] = pd.to_datetime(result["@timestamp"])
     return result.sort_values("@timestamp", ascending=False).reset_index(drop=True)
+
+def elastic_query_range(index,column,start_range,end_range=None,size=None,fields=None):
+    """
+Returns a pandas DataFrame of specified size and range of a specified column from the specified index.
+
+Args:
+    index: name of index in elasticsearch cluster
+    column: column name to be searched against
+    start_range: the start of the query range
+    end_range: the end of the query range, end_range = start_range if end_range == None (default: None)
+    size: size of response, returns all responses if None (default: None)
+    fields: list of fields to return, returns all fields if None (default: None)
+    
+    Examples:
+    df_date_range = elastic_query_range("paces3","@timestamp","2019-02-01","2019-02-03",None)
+    
+    """
+    
+    if end_range is None:
+        end_range = start_range
+    
+    if fields != None:
+        if "@timestamp" not in fields:
+            fields.append("@timestamp")
+    
+    body = {
+        "query": {
+            "range": {
+              column: {
+                "gte": start_range,
+                "lte": end_range
+              }
+            }
+        }
+    }
+    if fields == None:
+        response = es.search(index=index,body=body,request_timeout=120)
+    else:
+        response = es.search(index=index,body=body,request_timeout=120,_source=fields)
+    total_hits = response["hits"]["total"]["value"]
+    if size == None:
+        size = total_hits
+    if size <= 10000:
+        body = {
+            "size": size,
+            "query": {
+                "range": {
+                  column: {
+                    "gte": start_range,
+                    "lte": end_range
+                  }
+                }
+            }
+        }
+        if fields == None:
+            response = es.search(index=index,body=body,request_timeout=120)
+        else:
+            response = es.search(index=index,body=body,request_timeout=120,_source=fields)
+        return pd.DataFrame([response["hits"]["hits"][i]["_source"] for i in range(size)])
+    if total_hits <= 10000:
+        body = {
+            "size": total_hits,
+            "query": {
+                "range": {
+                  column: {
+                    "gte": start_range,
+                    "lte": end_range
+                  }
+                }
+            }
+        }
+        if fields == None:
+            response = es.search(index=index,body=body,request_timeout=120)
+        else:
+            response = es.search(index=index,body=body,request_timeout=120,_source=fields)
+        return pd.DataFrame([response["hits"]["hits"][i]["_source"] for i in range(total_hits)])
+    result = []
+    body = {
+        "size": 10000,
+        "query": {
+            "range": {
+              column: {
+                "gte": start_range,
+                "lte": end_range
+              }
+            }
+        }
+    }
+    if fields == None:
+        response = es.search(index=index,body=body,scroll="10s",request_timeout=120)
+    else:
+        response = es.search(index=index,body=body,scroll="10s",request_timeout=120,_source=fields)
+    response_hits = len(response["hits"]["hits"])
+    result.append(pd.DataFrame([response["hits"]["hits"][i]["_source"] for i in range(response_hits)]))
+    scroll_id = response["_scroll_id"]
+    for i in range(10000,size,10000):
+        response = es.scroll(scroll_id=scroll_id,scroll="10s")
+        response_hits = len(response["hits"]["hits"])
+        result.append(pd.DataFrame([response["hits"]["hits"][i]["_source"] for i in range(response_hits)]))
+    result = pd.concat(result,axis=0,ignore_index=True)
+    result["@timestamp"] = pd.to_datetime(result["@timestamp"])
+    return result.sort_values("@timestamp", ascending=False).reset_index(drop=True)
+
+def elastic_simple_aggs(index,query_type,query_params,aggs_type,aggs_params):
+    """
+Returns a json (python dictionary) of the specified aggregation result.
+
+Args:
+    index: name of index in elasticsearch cluster
+    query_type: "match" or "range", no query if None
+    query_params:
+        "match": {
+            "{{name_of_field}}": "{{query}}"
+        },
+        "range": {
+            "{{name_of_field}}": {
+                "gte": "{{start_range}}",
+                "lte": "{{end_range}}"
+            }
+        }
+    aggs_type: "cardinality", "terms", "date_histogram", "histogram", "min", "max", or "avg"
+    aggs_params:
+        "cardinality": {
+            "field": "{{name_of_field}}"
+        },
+        "terms": {
+            "field": "{{name_of_field}}",
+            "size": {{top_n_terms}}
+        },
+        "date_histogram" {
+            "field": "{{name_of_field}}",
+            "interval": "{{datetime_interval}}"
+        },
+        "histogram": {
+            "field": "{{name_of_field}}",
+            "interval": {{numeric_interval}}
+        },
+        "min": {
+            "field": "{{name_of_field}}"
+        },
+        "max": {
+            "field": "{{name_of_field}}"
+        },
+        "avg": {
+            "field": "{{name_of_field}}"
+        }
+    
+    """
+    if query_type != None:
+        body = {
+            "size": 0,
+            "query": {
+                query_type: query_params
+            },
+            "aggs": {}
+        }
+    else:
+        body = {
+            "size": 0,
+            "aggs": {}
+        }
+        
+    body["aggs"]["my_aggs"] = {
+        aggs_type: aggs_params
+    }
+    
+    response = es.search(index=index,body=body,request_timeout=120)
+    return response["aggregations"]["my_aggs"]
